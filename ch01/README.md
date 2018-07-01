e7747d08d72ad7affe7bcf24a7bf0b487fd45575
pollenjp <polleninjp@gmail.com>
2018-07-01 13:05:02 +0900

    First Commit!

---
+++ ch01/README.md
@@ -0,0 +1,6 @@
+
+
+
+- <a href="http://openglbook.com/the-book.html">the book - OpenGLBook.com</a>
+
+
---
+++ ch01/chapter.1.c
@@ -0,0 +1,98 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <GL/glew.h>
+#include <GL/freeglut.h>
+#define WINDOW_TITLE_PREFIX "Chapter 1"
+
+int CurrentWidth = 800,
+    CurrentHeight = 600,
+    WindowHandle = 0;
+
+void Initialize(int, char*[]);
+void InitWindow(int, char*[]);
+void ResizeFunction(int, int);
+void RenderFunction(void);
+
+//----------------------------------------
+// main
+//----------------------------------------
+int main(int argc, char* argv[])
+{
+  Initialize(argc, argv);
+
+  glutMainLoop();
+
+  exit(EXIT_SUCCESS);
+}
+
+//--------------------
+// Initialize
+//--------------------
+void Initialize(int argc, char* argv[])
+{
+  InitWindow(argc, argv);
+
+  fprintf(
+      stdout,
+      "INFO: OpenGL Version: %s\n",
+      glGetString(GL_VERSION)
+      );
+
+  glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+}
+
+//--------------------
+// InitWindow
+//--------------------
+void InitWindow(int argc, char* argv[])
+{
+  glutInit(&argc, argv);
+
+  glutInitContextVersion(4, 0);
+  glutInitContextFlags(GLUT_FORWARD_COMPATIBLE);
+  glutInitContextProfile(GLUT_CORE_PROFILE);
+
+  glutSetOption(
+      GLUT_ACTION_ON_WINDOW_CLOSE,
+      GLUT_ACTION_GLUTMAINLOOP_RETURNS
+      );
+
+  glutInitWindowSize(CurrentWidth, CurrentHeight);
+
+  glutInitDisplayMode(GLUT_DEPTH | GLUT_DOUBLE | GLUT_RGBA);
+
+  WindowHandle = glutCreateWindow(WINDOW_TITLE_PREFIX);
+
+  if(WindowHandle < 1) {
+    fprintf(
+        stderr,
+        "ERROR: Could not create a new rendering window.\n"
+        );
+    exit(EXIT_FAILURE);
+  }
+
+  glutReshapeFunc(ResizeFunction);
+  glutDisplayFunc(RenderFunction);
+}
+
+//--------------------
+// ResizeFunction
+//--------------------
+void ResizeFunction(int Width, int Height)
+{
+  CurrentWidth = Width;
+  CurrentHeight = Height;
+  glViewport(0, 0, CurrentWidth, CurrentHeight);
+}
+
+//--------------------
+// RenderFunction
+//--------------------
+void RenderFunction(void)
+{
+  glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+  glutSwapBuffers();
+  glutPostRedisplay();
+}
